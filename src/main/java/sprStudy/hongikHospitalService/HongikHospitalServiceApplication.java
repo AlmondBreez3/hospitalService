7a3a62cc4c73a712d616481bb5d089ff120f02c0
Hanbee Lee <bee1162@naver.com>
2023-05-20 02:23:31 +0900

    초기설정

---
+++ src/main/java/sprStudy/hongikHospitalService/HongikHospitalServiceApplication.java
@@ -0,0 +1,13 @@
+package sprStudy.hongikHospitalService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongikHospitalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongikHospitalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sprStudy/hongikHospitalService/HongikHospitalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sprStudy.hongikHospitalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongikHospitalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
